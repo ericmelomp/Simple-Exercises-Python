08675015783b981ffcd40dab83fa8252c6eb9e5d
Eric Melo <ericmelomp@gmail.com>
2022-08-12 17:36:57 -0300

    Criação exemplos básicos

---
+++ simple-full-name.py
@@ -0,0 +1,5 @@
+primeironome = input('Qual o seu primeiro nome? ')
+sobrenome = input('Qual o seu sobrenome? ')
+ultimonome = input('Qual o seu último nome? ')
+
+print('Seu nome completo é',primeironome,sobrenome,ultimonome)
