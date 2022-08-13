699c5f8a99a6993e32529c0f3c7e1d7dbee7e3f5
Eric Melo <ericmelomp@gmail.com>
2022-08-13 00:29:31 -0300

    Ajustes

---
+++ simple-age-calculate.py
@@ -0,0 +1,7 @@
+bornyear = int(input('''Em que ano você nasceu? 
+- '''))
+currentyear = int(input('''Em que ano estamos? 
+- '''))
+age = currentyear-bornyear
+
+print('Você tem ou fará {} anos de idade'.format(age))
